8a7c7dcf32f4d95905d4b32e87e3ec3cc0f93c68
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-23 14:45:16 +0630

    fix(): hydration error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,35 @@ import { PatientForm } from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 import { LogoImage } from "../components/LogoImage";
+import Mounted from "@/components/Mounted";
 
 export default function Home() {
   return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
-          <LogoImage />
-          <PatientForm />
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 Medicare
-            </p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
+    <Mounted>
+      <div className="flex h-screen max-h-screen">
+        <section className="remove-scrollbar container my-auto">
+          <div className="sub-container max-w-[496px]">
+            <LogoImage />
+            <PatientForm />
+            <div className="text-14-regular mt-20 flex justify-between">
+              <p className="justify-items-end text-dark-600 xl:text-left">
+                © 2024 Medicare
+              </p>
+              <Link href="/?admin=true" className="text-green-500">
+                Admin
+              </Link>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <Image
-        src="/assets/images/onboarding-img.png"
-        height={1000}
-        width={1000}
-        alt="patient"
-        className="side-img max-w-[50%]"
-      />
-    </div>
+        <Image
+          src="/assets/images/onboarding-img.png"
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="side-img max-w-[50%]"
+        />
+      </div>
+    </Mounted>
   );
 }
