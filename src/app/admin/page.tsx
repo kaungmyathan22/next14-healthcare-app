0536d9da0c7549fba1f5dcbd523e346b82814399
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-24 13:58:57 +0630

    feat(): import path fixed

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,10 @@
 import { LogoImage } from "@/components/LogoImage";
 import Mounted from "@/components/Mounted";
 import { StatCard } from "@/components/StatCard";
+import { DataTable } from "@/components/table/DataTable";
+import { columns } from "@/components/table/columns";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import Link from "next/link";
-import { getRecentAppointmentList } from "../../../original/lib/actions/appointment.actions";
-import { DataTable } from "../../../original/components/table/DataTable";
-import { columns } from "../../../original/components/table/columns";
 
 const Admin = async () => {
   const appointments = await getRecentAppointmentList();
