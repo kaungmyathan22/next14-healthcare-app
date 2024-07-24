fc0ccd52333c99058d4bcfc9e3d89e1a73c37e13
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-24 10:51:46 +0630

    fix(): hydration error

--- src/app/patients/[userId]/register/page.tsx
+++ src/app/patients/[userId]/register/page.tsx
@@ -1,4 +1,5 @@
 import { LogoImage } from "@/components/LogoImage";
+import Mounted from "@/components/Mounted";
 import RegisterForm from "@/components/forms/RegisterForm";
 import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
@@ -7,23 +8,25 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
 
   return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
-          <LogoImage />
-          <RegisterForm user={user} />
-          <p className="copyright py-12">© 2024 Medicare</p>
-        </div>
-      </section>
+    <Mounted>
+      <div className="flex h-screen max-h-screen">
+        <section className="remove-scrollbar container">
+          <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+            <LogoImage />
+            <RegisterForm user={user} />
+            <p className="copyright py-12">© 2024 Medicare</p>
+          </div>
+        </section>
 
-      <Image
-        src="/assets/images/register-img.png"
-        height={1000}
-        width={1000}
-        alt="patient"
-        className="side-img max-w-[390px]"
-      />
-    </div>
+        <Image
+          src="/assets/images/register-img.png"
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="side-img max-w-[390px]"
+        />
+      </div>
+    </Mounted>
   );
 };
 
